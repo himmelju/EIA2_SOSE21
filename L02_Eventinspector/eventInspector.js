b55ac94431a7125d8bb9e56dc2dc6f0fcd573c1f
himmelju <julian.himmel@t-online.de>
2021-04-16 15:26:38 +0200

    ts, css, update

--- L02_Eventinspector/eventInspector.js
+++ L02_Eventinspector/eventInspector.js
@@ -2,16 +2,24 @@
 var eventInspector;
 (function (eventInspector) {
     window.addEventListener("load", handleLoad);
-    function handleLoad() {
-        document.addEventListener("mousemove", function () {
-            handleLoad();
-        });
-        document.addEventListener("click", function () {
-            handleLoad();
-        });
-        document.addEventListener("keyup", function () {
-            handleLoad();
-        });
+    function handleLoad(_event) {
+        document.addEventListener("mousemove", setInfoBox);
+        let body = document.querySelector("body");
+        body.addEventListener("keyup", logInfo);
+        body.addEventListener("click", logInfo);
+    }
+    function setInfoBox(_event) {
+        let x = _event.pageX;
+        let y = _event.pageY;
+        let spanForMouseCursor = document.querySelector("span");
+        spanForMouseCursor.style.left = x + "px";
+        spanForMouseCursor.style.top = x + "px";
+    }
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);
     }
 })(eventInspector || (eventInspector = {}));
 //# sourceMappingURL=eventInspector.js.map
--- L02_Eventinspector/eventInspector.ts
+++ L02_Eventinspector/eventInspector.ts
@@ -2,16 +2,28 @@ namespace eventInspector {
 
     window.addEventListener("load", handleLoad);     
     
-    function handleLoad(): void {
+    function handleLoad(_event): void {
 
-        document.addEventListener("mousemove", function (): void {
-            handleLoad();
-        });
-        document.addEventListener("click", function (): void {
-            handleLoad();
-        });
-        document.addEventListener("keyup", function (): void {
-            handleLoad();
-        });
+        document.addEventListener("mousemove", setInfoBox);
+        let body = document.querySelector("body");
+        body.addEventListener("keyup", logInfo);
+        body.addEventListener("click", logInfo);
+
     } 
+
+    function setInfoBox(_event) {
+        let x = _event.pageX;
+        let y = _event.pageY;
+        let spanForMouseCursor = document.querySelector("span");
+        spanForMouseCursor.style.left = x + "px";
+        spanForMouseCursor.style.top = x + "px";
+    }
+
+    function logInfo(_event) {
+        console.log(_event.type);
+        console.log(_event.target);
+        console.log(_event.currentTarget);
+        console.log(_event);  
+    }
+
 }
