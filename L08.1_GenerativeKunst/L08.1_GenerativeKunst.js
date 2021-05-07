7a07a8b8c0d833d4d4c3251e56630f29d9dd3c2f
himmelju <julian.himmel@t-online.de>
2021-05-07 16:31:26 +0200

    lines

--- L08.1_GenerativeKunst/L08.1_GenerativeKunst.js
+++ L08.1_GenerativeKunst/L08.1_GenerativeKunst.js
@@ -3,13 +3,8 @@ var GenerativeKunst;
 (function (GenerativeKunst) {
     window.addEventListener("load", createCanvas);
     let crc2;
-    function createCanvas() {
-        let canvas = document.querySelector("canvas");
-        crc2 = canvas.getContext("2d");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        drawArtDots();
-    }
+    let size = 320;
+    let step = 80;
     function drawArtDots() {
         for (let i = 0; i < 50; i++) {
             let purple = Math.floor(Math.random() * 200);
@@ -22,6 +17,34 @@ var GenerativeKunst;
             crc2.fillStyle = "rgb(" + purple + ", " + yellow + "," + blue + ")";
             crc2.fill();
         }
+    }
+    function backgroundLines(x, y, innerWidth, innerHeight) {
+        for (var x = 0; x < size; x += step) {
+            for (var y = 0; y < size; y += step) {
+                let white = Math.floor(Math.random() * 300);
+                let leftToRight = Math.random() >= 0.5;
+                if (leftToRight) {
+                    crc2.moveTo(x, y);
+                    crc2.lineTo(x + innerWidth, y + innerHeight);
+                }
+                else {
+                    crc2.moveTo(x + innerWidth, y);
+                    crc2.lineTo(x, y + innerHeight);
+                }
+                crc2.stroke();
+                crc2.strokeStyle = "rgb(" + white + ")";
+            }
+        }
+    }
+    function createCanvas() {
+        let canvas = document.querySelector("canvas");
+        crc2 = canvas.getContext("2d");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        crc2.lineCap = "square";
+        crc2.lineWidth = 2;
+        drawArtDots();
+        backgroundLines(0, 0, innerWidth, innerHeight);
     }
 })(GenerativeKunst || (GenerativeKunst = {}));
 //# sourceMappingURL=L08.1_GenerativeKunst.js.map
--- L08.1_GenerativeKunst/L08.1_GenerativeKunst.ts
+++ L08.1_GenerativeKunst/L08.1_GenerativeKunst.ts
@@ -3,15 +3,8 @@ namespace GenerativeKunst {
     window.addEventListener("load", createCanvas);
 
     let crc2: CanvasRenderingContext2D;
-
-    function createCanvas(): void {
-        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
-        crc2 = <CanvasRenderingContext2D> canvas.getContext("2d");
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        
-        drawArtDots();
-    }
+    let size: number = 320;
+    let step: number = 80;
 
     function drawArtDots(): void {
         for (let i: number = 0; i < 50; i++) {
@@ -30,5 +23,35 @@ namespace GenerativeKunst {
         }
     }
 
+    function backgroundLines(x: number, y: number, innerWidth: number, innerHeight: number): void {
+        for (var x: number = 0; x < size; x += step) {
+            for (var y: number = 0; y < size; y += step) {
 
+                let white: number = Math.floor(Math.random() * 300);
+                
+                let leftToRight: boolean = Math.random() >= 0.5;
+                
+                if (leftToRight) {
+                    crc2.moveTo(x, y);
+                    crc2.lineTo(x + innerWidth, y + innerHeight);
+                } else {
+                    crc2.moveTo(x + innerWidth, y);
+                    crc2.lineTo(x, y + innerHeight);
+                }
+                crc2.stroke();
+                crc2.strokeStyle = "rgb(" + white + ")";
+            }    
+        }
+    }    
+
+    function createCanvas(): void {
+        let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
+        crc2 = <CanvasRenderingContext2D> canvas.getContext("2d");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        crc2.lineCap = "square";
+        crc2.lineWidth = 2;
+        drawArtDots();
+        backgroundLines(0, 0, innerWidth, innerHeight);
+    }
 }
