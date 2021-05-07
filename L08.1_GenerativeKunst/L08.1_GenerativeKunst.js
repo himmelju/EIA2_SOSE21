402af0f76c40da272ddcc2ab75fce31cddaa4580
himmelju <julian.himmel@t-online.de>
2021-05-07 14:18:45 +0200

    dots work

--- L08.1_GenerativeKunst/L08.1_GenerativeKunst.js
+++ L08.1_GenerativeKunst/L08.1_GenerativeKunst.js
@@ -6,18 +6,20 @@ var GenerativeKunst;
     function createCanvas() {
         let canvas = document.querySelector("canvas");
         crc2 = canvas.getContext("2d");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
         drawArtDots();
     }
     function drawArtDots() {
         for (let i = 0; i < 50; i++) {
-            let red = Math.floor(Math.random() * 300);
-            let green = Math.floor(Math.random() * 300);
-            let blue = Math.floor(Math.random() * 300);
+            let purple = Math.floor(Math.random() * 200);
+            let yellow = Math.floor(Math.random() * 400);
+            let blue = Math.floor(Math.random() * 600);
             crc2.beginPath();
-            crc2.arc(Math.floor(Math.random() * (1000) + 1), Math.floor(Math.random() * (700) + 1), Math.floor(Math.random() * (90) + 1), 0, 2 * Math.PI);
+            crc2.arc(Math.floor(Math.random() * (2500) + 1), Math.floor(Math.random() * (1500) + 1), Math.floor(Math.random() * (100) + 10), 0, 2 * Math.PI);
             crc2.stroke();
             crc2.closePath();
-            crc2.fillStyle = "rgb(" + red + ", " + green + "," + blue + ")";
+            crc2.fillStyle = "rgb(" + purple + ", " + yellow + "," + blue + ")";
             crc2.fill();
         }
     }
--- L08.1_GenerativeKunst/L08.1_GenerativeKunst.ts
+++ L08.1_GenerativeKunst/L08.1_GenerativeKunst.ts
@@ -7,23 +7,26 @@ namespace GenerativeKunst {
     function createCanvas(): void {
         let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.querySelector("canvas");
         crc2 = <CanvasRenderingContext2D> canvas.getContext("2d");
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
         
         drawArtDots();
     }
 
     function drawArtDots(): void {
         for (let i: number = 0; i < 50; i++) {
-            let red: number = Math.floor(Math.random() * 300);
-            let green: number = Math.floor(Math.random() * 300);
-            let blue: number = Math.floor(Math.random() * 300);
+            let purple: number = Math.floor(Math.random() * 200);
+            let yellow: number = Math.floor(Math.random() * 400);
+            let blue: number = Math.floor(Math.random() * 600);
             crc2.beginPath();
 
-            crc2.arc(Math.floor(Math.random() * (1000) + 1), Math.floor(Math.random() * (700) + 1), Math.floor(Math.random() * (90) + 1), 0, 2 * Math.PI);
+            crc2.arc(Math.floor(Math.random() * (2500) + 1), Math.floor(Math.random() * (1500) + 1), Math.floor(Math.random() * (100) + 10), 0, 2 * Math.PI);
             crc2.stroke();
             crc2.closePath();
 
-            crc2.fillStyle = "rgb(" + red + ", " + green + "," + blue + ")";
+            crc2.fillStyle = "rgb(" + purple + ", " + yellow + "," + blue + ")";
             crc2.fill();
+            
         }
     }
 
